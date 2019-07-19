13ac6aee9492dfaf91f16af366b35cb0f9560673
Matthew Mbonu <mcmbonu1@gmail.com>
2019-07-19 11:59:26 -0400

    api.js

---
+++ shopping-list-starter-master/scripts/api.js
@@ -0,0 +1,21 @@
+const api = (function() {
+  const BASE_URL = "https://thinkful-list-api.herokuapp.com/Matt-And";
+  const getItems = () => {
+    return fetch(`${BASE_URL}/items`);
+  };
+
+  const createItem = name => {
+    let newItem = { name };
+    newItem = JSON.stringify(newItem);
+    return fetch(`${BASE_URL}/items`, {
+      method: "POST",
+      headers: new Headers({ "Content-Type": "application/json" }),
+      body: newItem
+    });
+  };
+
+  return {
+    getItems,
+    createItem
+  };
+})();
