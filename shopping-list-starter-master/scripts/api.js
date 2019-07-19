0847f1a35459e25d11c6c446d5b76936f192e91b
Andrew Jessen-Tyler <andrewtyl@gmail.com>
2019-07-19 10:21:25 -0500

    createItem() method

--- shopping-list-starter-master/scripts/api.js
+++ shopping-list-starter-master/scripts/api.js
@@ -3,7 +3,15 @@ const api = (function() {
   const getItems = () => {
     return fetch(`${BASE_URL}/items`);
   };
+
+  const createItem = (name) => {
+    let newItem = {name};
+    newItem = JSON.stringify(newItem);
+    return fetch(`${BASE_URL}/items`, {method: "POST", headers: new Headers({ 'Content-Type': 'application/json' }), body: newItem});
+  }
+
+
   return {
-    getItems
+    getItems, createItem
   };
 })();
